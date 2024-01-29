60403da84e1e5e1080af49945d6f8ddd098df463
Parker <parker.arneson@gmail.com>
2024-01-28 21:06:09 -0500

    Added functionality to tabs

--- app/src/main/java/com/example/projectone_cs2340/MainActivity.java
+++ app/src/main/java/com/example/projectone_cs2340/MainActivity.java
@@ -1,20 +1,62 @@
 package com.example.projectone_cs2340;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.projectone_cs2340.Scheduler.Schedule;
+import com.example.projectone_cs2340.Scheduler.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calendar_fragment);
+
+        createLists(savedInstanceState);
 
         Schedule schedule = new Schedule("C:\\testData.txt");
+    }
+
+    private void createLists(Bundle savedInstanceState) {
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager viewPager = findViewById(R.id.pager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+        );
+
+        ItemList calendarListView = new ItemList();
+        ItemList todoList = new ItemList();
+
+
+        viewPageAdapter.addFragment(calendarListView, "Events");
+        viewPageAdapter.addFragment(todoList, "Todo");
+
+        todoList.addToList(new Task("CS2340", "Finish project"));
+        todoList.addToList(new Task("MATH1554", "Study for test"));
+        todoList.addToList(new Task("PSYC1101", "Study for test," +
+                " review study guide"));
+        todoList.addToList(new Task("Personal", "Get mixers from Publix"));
+
+        viewPager.setAdapter(viewPageAdapter);
     }
 }
--- app/src/main/java/com/example/projectone_cs2340/Scheduler/Task.java
+++ app/src/main/java/com/example/projectone_cs2340/Scheduler/Task.java
@@ -4,13 +4,29 @@ import android.view.View;
 
 public class Task extends Event {
     private boolean completed;
+    private String title;
+    private String description;
 
-    public Task(String data) {
+    //public Task(String data) {
+    //    String[] dataArray = data.split(",");
+    //    title = dataArray[1];
+    //    description = dataArray[2];
+    //    completed = Boolean.parseBoolean(dataArray[3]);
+    //}
 
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
     }
-
     @Override
     public void createView(View view) {
 
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
     }
 }
