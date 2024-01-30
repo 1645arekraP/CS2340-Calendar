62f3aea990ef1a1d2488835e7eefe47176f6a3dd
Parker <parker.arneson@gmail.com>
2024-01-30 01:32:21 -0500

    Combined features

--- app/src/main/java/com/example/projectone_cs2340/MainActivity.java
+++ app/src/main/java/com/example/projectone_cs2340/MainActivity.java
@@ -1,29 +1,23 @@
 package com.example.projectone_cs2340;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-
-import com.example.projectone_cs2340.Scheduler.Schedule;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.example.projectone_cs2340.Scheduler.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-import java.util.ArrayList;
+import androidx.appcompat.app.AlertDialog;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton eventButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +25,52 @@ public class MainActivity extends AppCompatActivity {
 
         createLists(savedInstanceState);
 
-        Schedule schedule = new Schedule("C:\\testData.txt");
+        eventButton = findViewById(R.id.todoButton);
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, v);
+                popupMenu.getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.one) {
+                            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.fragment_pop_up, null);
+                            EditText eventName = (EditText) view.findViewById(R.id.name);
+                            EditText eventDate = (EditText) view.findViewById(R.id.time);
+                            EditText courseName = (EditText) view.findViewById(R.id.course);
+                            EditText location = (EditText) view.findViewById(R.id.location);
+                            EditText professorName = (EditText) view.findViewById(R.id.professor);
+                            EditText isCompleted = (EditText) view.findViewById(R.id.completed);
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage("Enter event details here:")
+                                    .setView(view)
+                                    .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            String eName = eventName.getText().toString();
+                                            String eDate = eventDate.getText().toString();
+                                            String cName = courseName.getText().toString();
+                                            String loc = location.getText().toString();
+                                            String prof = professorName.getText().toString();
+                                            String complete = isCompleted.getText().toString();
+
+                                            Toast myToast = Toast.makeText(MainActivity.this, "Hello toast!", Toast.LENGTH_SHORT);
+                                            myToast.show();
+                                        }
+                                    })
+                                    .setNegativeButton("Cancel", null)
+                                    .setCancelable(false);
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                        }
+                        return false;
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
     }
 
     private void createLists(Bundle savedInstanceState) {
