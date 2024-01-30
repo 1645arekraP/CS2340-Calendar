f7d1c27b7c6d8bcdcd429caf148129ff7e84397e
Aryan Patel <aaryanspatel.2004@yahoo.com>
2024-01-30 01:58:25 -0500

    Fixed Main Button and added functionalities for each PopUp.

--- app/src/main/java/com/example/projectone_cs2340/AssignmentPopUp.java
+++ app/src/main/java/com/example/projectone_cs2340/AssignmentPopUp.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PopUp#newInstance} factory method to
+ * Use the {@link AssignmentPopUp#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PopUp extends Fragment {
+public class AssignmentPopUp extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class PopUp extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PopUp() {
+    public AssignmentPopUp() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class PopUp extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PopUp.
+     * @return A new instance of fragment AssignmentPopUp.
      */
     // TODO: Rename and change types and number of parameters
-    public static PopUp newInstance(String param1, String param2) {
-        PopUp fragment = new PopUp();
+    public static AssignmentPopUp newInstance(String param1, String param2) {
+        AssignmentPopUp fragment = new AssignmentPopUp();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class PopUp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.course_pop_up, container, false);
+        return inflater.inflate(R.layout.assignment_pop_up, container, false);
     }
 }
